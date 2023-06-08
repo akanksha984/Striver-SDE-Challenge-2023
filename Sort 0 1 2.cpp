fcbb4b388d40db4858e16caa06fc4227ef2b54a0
akanksha984 <97845909+akanksha984@users.noreply.github.com>
2023-06-08 18:35:12 +0530

    Update Sort 0 1 2.cpp

--- Sort 0 1 2.cpp
+++ Sort 0 1 2.cpp
@@ -15,3 +15,29 @@ void sort012(int *arr, int n)
       else arr[i]=2;
    }
 }
+
+// https://leetcode.com/problems/sort-colors/description/
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int c0=0, c1=0;
+        int n= nums.size();
+        for (int i=0; i<n; i++){
+            if (nums[i]==0)c0++;
+            else if (nums[i]==1)c1++;
+        }
+        for (int i=0; i<n; i++){
+            if (c0>0){
+                nums[i]=0;
+                c0--;
+            }
+            else if (c1>0){
+                nums[i]=1;
+                c1--;
+            }
+            else{
+                nums[i]=2;
+            }
+        }
+    }
+};
