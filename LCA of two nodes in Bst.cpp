a40fa1499421a1dbe92cb804837eb676cb3fa2f7
akanksha984 <97845909+akanksha984@users.noreply.github.com>
2023-06-14 17:08:37 +0530

    Create LCA of two nodes in Bst.cpp

---
+++ LCA of two nodes in Bst.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h> 
+/************************************************************
+    Following is the Binary Search Tree node structure
+    
+    template <typename T>
+    class TreeNode {
+        public :
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+
+        ~TreeNode() {
+            if(left)
+                delete left;
+            if(right)
+                delete right;
+        }
+    };
+
+************************************************************/
+
+TreeNode<int>* LCAinaBST(TreeNode<int>* root, TreeNode<int>* P, TreeNode<int>* Q)
+{
+	// Write your code here
+    TreeNode<int>* ans= NULL;
+    while (root){
+        if (root->data > P->data && root->data > Q->data){
+            root= root->left;
+        }
+        else if (root->data < P->data && root->data < Q->data){
+            root= root->right;
+        }
+        else return root;
+    }
+    return ans;
+}
