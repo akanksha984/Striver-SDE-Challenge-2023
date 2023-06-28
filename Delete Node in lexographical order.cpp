933b4d1267a7715d1ca4f0868bcbbde599e5460a
akanksha984 <97845909+akanksha984@users.noreply.github.com>
2023-06-29 00:45:50 +0530

    Create Delete Node in lexographical order.cpp

---
+++ Delete Node in lexographical order.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+/****************************************************************
+
+    Following is the class structure of the LinkedListNode class:
+
+    template <typename T>
+    class LinkedListNode
+    {
+    public:
+        T data;
+        LinkedListNode<T> *next;
+        LinkedListNode(T data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+*****************************************************************/
+
+void deleteNode(LinkedListNode<int> * node) {
+    // Write your code here.
+    int nxt= node->next->data;
+    node->data= nxt;
+    node->next= node->next->next;
+
+}
