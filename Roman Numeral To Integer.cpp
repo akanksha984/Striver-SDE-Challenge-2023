fcf68a40311f3f6de3715d9d05ff24e88b9ff68d
akanksha984 <97845909+akanksha984@users.noreply.github.com>
2023-07-01 21:04:49 +0530

    Create Roman Numeral To Integer.cpp

---
+++ Roman Numeral To Integer.cpp
@@ -0,0 +1,15 @@
+int romanToInt(string s) {
+    // Write your code here
+    if (s.size()==0)return 0;
+      unordered_map<char,int>mp{{'M',1000},{'D',500},{'C',100},{'L',50},{'X',10},{'V',5},{'I',1}};
+    int ans= 0;
+    for (int i=0; i<s.size(); i++){
+        if (mp[s[i]]<mp[s[i+1]]){
+            ans-= mp[s[i]];
+        }
+        else{
+            ans+= mp[s[i]];
+        }
+    }
+    return ans;
+}
